29e2473c5d9cb4ffed976e50afbd6e2b4c773b07
kylejvh <kyvhy66@gmail.com>
2019-11-06 12:48:38 -0500

    Updates

---
+++ precache-manifest.b1ad08c961d245abaf8b996d39a4fedf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c80e07367eb3c95b2e66b9605ef07e",
+    "url": "/React-MovieCards/index.html"
+  },
+  {
+    "revision": "73f8bc5999cafcf99a9a",
+    "url": "/React-MovieCards/static/css/main.5d264cc7.chunk.css"
+  },
+  {
+    "revision": "8c744efd5834819f308f",
+    "url": "/React-MovieCards/static/js/2.8240196f.chunk.js"
+  },
+  {
+    "revision": "73f8bc5999cafcf99a9a",
+    "url": "/React-MovieCards/static/js/main.7bc9e834.chunk.js"
+  },
+  {
+    "revision": "2eb65a995d7f3b11f9eb",
+    "url": "/React-MovieCards/static/js/runtime-main.2caa9eb0.js"
+  }
+]);
