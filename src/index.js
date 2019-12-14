45eb313d3db396f076a5c688ea52a560a7180845
kylejvh <kyvhy66@gmail.com>
2019-12-14 15:41:33 -0500

    Changes to support gh-pages deployment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.querySelector("#root")
