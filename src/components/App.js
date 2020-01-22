eb9ef4e114b67409f386e4a928991d870376c23a
kylejvh <49110126+kylejvh@users.noreply.github.com>
2020-01-22 00:23:55 -0500

    Deploy Tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,6 @@ import { hot } from "react-hot-loader/root";
 import { Router } from "@reach/router";
 import React from "react";
 
-import { createGlobalStyle } from "styled-components";
-
-import { normalize } from "styled-normalize";
-
 import Store from "./Store/Store";
 import Search from "../pages/Search";
 import FullMoviePage from "../pages/FullMoviePage";
@@ -14,22 +10,9 @@ import UpcomingMovies from "../pages/UpcomingMovies";
 import Favorites from "../pages/Favorites";
 // import DiscoverByActor from "../pages/DiscoverByActor";
 
-const GlobalStyle = createGlobalStyle`
-  ${normalize}
-
-  body {
-    @import url("https://fonts.googleapis.com/css?family=Titillium+Web:400, 600i&display=swap");
-    @import url("https://fonts.googleapis.com/css?family=Nunito&display=swap");
-    /* font-family: "Nunito", sans-serif; */
-    background-color: #2c3949;
-  font-family: "Titillium Web";  
-  }
-`;
-
 const App = () => {
   return (
     <Store>
-      <GlobalStyle />
       <Router>
         <PopularMovies path="/" />
         <UpcomingMovies path="/upcoming" />
